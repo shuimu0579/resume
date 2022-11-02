88c7a316b03b18539dbd269ee795f1f2921825a5
吴习章 <wuxz14@vanke.com>
2022-11-02 21:39:30 +0800

    feat: init change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  base:"suimulearn"
+  base:"suimulearn",
+  integrations: [solidJs(), tailwind()]
 });
---
+++ meta.config.js
@@ -0,0 +1,4 @@
+export default {
+  name: "吴习章",
+  skills: ["Vue3", "js", "threejs", "solidjs"],
+};
