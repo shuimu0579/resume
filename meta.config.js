533aaffe922f0bdd8bf60c28c0949ec5e12bb0a2
吴习章 <wuxz14@vanke.com>
2022-11-09 21:50:08 +0800

    init change

--- meta.config.js
+++ meta.config.js
@@ -1,5 +1,9 @@
 export default {
   name: "吴习章",
+  workExperience: '工作经历',
+  projectExperience: '项目经历',
+  skillExperience: '工作技能',
+  educationExperience: '教育经历',
   skills: ["Vue3", "js", "threejs", "solidjs"],
   education:["2009.9-2013.6", "湖北文理学院", "土木工程"]
 };
