3b7542b38a692ec7eeed019026628a05891d4890
吴习章 <wuxz14@vanke.com>
2022-11-09 22:11:41 +0800

    init change

--- meta.config.js
+++ meta.config.js
@@ -4,6 +4,6 @@ export default {
   projectExperience: '项目经历',
   skillExperience: '工作技能',
   educationExperience: '教育经历',
-  skills: ["Vue3", "js", "threejs", "solidjs"],
+  skills: ["Vue3", "React", "Threejs", "Vite"],
   education:["2009.9-2013.6", "湖北文理学院", "土木工程"]
 };
